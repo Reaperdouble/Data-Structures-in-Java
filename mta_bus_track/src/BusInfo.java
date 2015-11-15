78fe60e762dfd0efcd212a54cbd51f7b5fc84af1
radra_000 <radra_000@rad>
2015-11-15 11:59:37 -0500

    added few more variables to obtain information regarding bus location with respect to the bus stop

--- mta_bus_track/src/BusInfo.java
+++ mta_bus_track/src/BusInfo.java
@@ -3,7 +3,9 @@ import java.io.Serializable;
 
 
 public class BusInfo implements Serializable{
-    private int timeRemaining;
+    private String distanceFromCall;
+    private String presentableDistance;
+    private String vehicleRef;
     private int stopsAway;
     private String busName;
     private String destination;
@@ -14,8 +16,10 @@ public class BusInfo implements Serializable{
     private double latitude;
     private double longitude;
 
-    public BusInfo(int timeRemaining, int stopsAway, String busName, String destination, String originId, String destinationId, String currentStop, String currentStopId, double latitude, double longitude) {
-        this.timeRemaining = timeRemaining;
+    public BusInfo(String vehicleRef, String distanceFromCall, String presentableDistance, int stopsAway, String busName, String destination, String originId, String destinationId, String currentStop, String currentStopId, double latitude, double longitude) {
+        this.vehicleRef = vehicleRef;
+        this.distanceFromCall = distanceFromCall;
+        this.presentableDistance = presentableDistance;
         this.stopsAway = stopsAway;
         this.busName = busName;
         this.destination = destination;
@@ -26,22 +30,31 @@ public class BusInfo implements Serializable{
         this.latitude = latitude;
         this.longitude = longitude;
     }
-    public void updateLocation(int timeRemaining, int stopsAway, String currentStop, String currentStopId, double latitude, double longitude){
-        this.timeRemaining = timeRemaining;
+    public void updateLocation(String presentableDistance, String distanceFromCall, int stopsAway, String currentStop, String currentStopId, double latitude, double longitude){
+        this.presentableDistance = presentableDistance;
+        this.distanceFromCall = distanceFromCall;
         this.stopsAway = stopsAway;
         this.currentStop = currentStop;
         this.currentStopId = currentStopId;
         this.latitude = latitude;
         this.longitude = longitude;
     }
-    public int getTimeRemaining() {
-        return timeRemaining;
+
+    public String getDistanceFromCall() {
+        return distanceFromCall;
     }
 
-    public double getStopsAway() {
+    public String getPresentableDistance() {
+        return presentableDistance;
+    }
+
+    public String getVehicleRef() {
+        return vehicleRef;
+    }
+
+    public int getStopsAway() {
         return stopsAway;
     }
-
     public String getBusName() {
         return busName;
     }
@@ -78,7 +91,7 @@ public class BusInfo implements Serializable{
 
         return(String.format("%10s",busName) + " | $" + 
                 String.format("%10s", stopsAway)+" | "+
-                String.format("%25s", timeRemaining)+ " | "+ 
+                String.format("%25s", presentableDistance)+ " | "+ 
                 String.format("%25s", currentStop)+ " | "+
                // String.format("%10s",timeRemaining+" hours")+ " | "+
                 String.format("%25s", destination));
