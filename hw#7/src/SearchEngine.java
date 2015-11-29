a9dab5770ba93771091651c39a3c538c45bb9cbc
radra_000 <radra_000@rad>
2015-11-29 14:09:22 -0500

    still building the class

--- hw#7/src/SearchEngine.java
+++ hw#7/src/SearchEngine.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -6,28 +8,90 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author radra_000
  */
 public class SearchEngine {
-    public static final String PAGES_FILE ="pages.txt";
+
+    public static final String PAGES_FILE = "pages.txt";
     public static final String LINKS_FILE = "links.txt";
-    private WebGraph web;
-    Scanner input = new Scanner(System.in);
-    public static void main(String[] args){
-        System.out.println("(AP) - Add a new page to the graph."
-                + "\n(RP) - Remove a page from the graph"
-                + "\n(AL) - Add a link between pages in the graph"
-                + "\n(RM) - Remove a link between pages in the graph"
-                + "\n(P) - Print the graph."
-                + "\n\t(I) - Sort based on index (ASC)"
-                + "\n\t(U) - Sort based on URL (ASC)"
-                + "\n\t(R) - Sort based on Rank (DSC)"
-                + "\n\t(Q) - Quit (return to previous menu)"
-                + "\n(S) - Search for pages with a keywork"
-                + "\n(Q) - Quit");
-    
+
+    public static void main(String[] args) {
+        WebGraph web = new WebGraph();
+        Scanner input = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.println("Menu"
+                    + "\n\t(AP) - Add a new page to the graph."
+                    + "\n\t(RP) - Remove a page from the graph"
+                    + "\n\t(AL) - Add a link between pages in the graph"
+                    + "\n\t(RM) - Remove a link between pages in the graph"
+                    + "\n\t(P) - Print the graph."
+                    + "\n\t(S) - Search for pages with a keywork"
+                    + "\n\t(Q) - Quit"
+                    + "\nSelect an Option: ");
+            String entry = input.nextLine().toUpperCase();
+            switch (entry) {
+                case ("AP"):
+                    System.out.println("Enter a URL: ");
+                    String url = input.nextLine();
+                    System.out.println("Enter keywords(space - seperated): ");
+                    ArrayList<String> keywords = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
+                    web.addPage(url, keywords);
+                    System.out.println(url + " succesfully added to the WebGraph");
+                    break;
+                case ("AL"):
+                    System.out.println("Enter a source URL: ");
+                    String source = input.nextLine();
+                    System.out.println("Enter a destination URL: ");
+                    String destination = input.nextLine();
+                    web.addlink(source, destination);
+                    System.out.println("Link Sucessfully added from " + source + " to " + destination + "!");
+                    break;
+                case ("RP"):
+                    System.out.println("Enter a URL");
+                    String webUrl = input.nextLine();
+                    web.removePage(webUrl);
+                    System.out.println(webUrl+" has been removed from the graph");
+                    break;
+                case ("RL"):
+                    System.out.println("Enter a source URL: ");
+                    String source1 = input.nextLine();
+                    System.out.println("Enter a destination URL: ");
+                    String destination1 = input.nextLine();
+                    web.removeLink(source1, destination1);
+                    System.out.println("Link removed from "+source1+" to "+destination1+"!");
+                    break;
+                case ("P"):
+                    System.out.println("\t(I) - Sort based on index (ASC)"
+                            + "\n\t(U) - Sort based on URL (ASC)"
+                            + "\n\t(R) - Sort based on Rank (DSC)"
+                            + "\n\t(Q) - Quit (return to previous menu)");
+                    entry = input.nextLine().toUpperCase();
+                    switch (entry) {
+                        case ("I"):
+                            break;
+                        case ("U"):
+                            break;
+                        case ("R"):
+                            break;
+                        case ("Q"):
+                            break;
+                    }
+                    web.printTable();
+                    break;
+                case ("S"):
+                    System.out.println("Search Keyword: ");
+                    String keyword = input.nextLine();
+                    String output = web.search(keyword);
+                    System.out.println(output);
+                    break;
+                case ("Q"):
+                    System.out.println("Program terminating....");
+                    run = false;
+                    break;
+            }
+        }
     }
 }
